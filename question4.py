1b30aa6a19f109b7654bc0af3eba00022b10b00f
Shintaro Takahashi <shinta8105@gmail.com>
2021-05-26 16:06:15 +0900

    all done

--- question4.py
+++ question4.py
@@ -1,17 +1,19 @@
 import sys 
 import datetime
 
-def main(logfile, N, m, t):
+def main(logfile, N):
     with open(logfile) as f:
         log_list = [r.strip().split(',') for r in f.readlines()]
-        eatch_server_logs = {}
+        eatch_subnet_logs = {}
         for log in log_list:
-            eatch_server_logs[log[1]] = []
+            network_len = int(log[1].split('/')[1]) / 8
+            network_area = ','.join(log[1].split('.')[:int(network_len)])
+            eatch_subnet_logs[network_area] = []
             for log2 in log_list:
-                if log[1] == log2[1]:
-                    eatch_server_logs[log[1]].append(log2)
+                if network_area == ','.join(log2[1].split('.')[:int(network_len)]):
+                    eatch_subnet_logs[network_area].append(log2)
         
-        for logs in eatch_server_logs.values():
+        for logs in eatch_subnet_logs.values():
             timeout_count = 0
             for i in range(len(logs)):
                 if logs[i][-1] == '-':
@@ -24,13 +26,8 @@ def main(logfile, N, m, t):
                                 timeout_count = 0
                         else:
                             print(logs[i][1], '-')
-                elif i >= int(m)-1:
-                    last_m_list = [int(l[-1]) for l in logs[i+1-int(m):i+1] if l[-1] != '-']
-                    return_time_avg = sum(last_m_list) / len(last_m_list)
-                    if return_time_avg > int(t):
-                        print(logs[i][1], return_time_avg)
 
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    main(sys.argv[1], sys.argv[2])
